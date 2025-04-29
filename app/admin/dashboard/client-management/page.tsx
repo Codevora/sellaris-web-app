30ca5919c57cf09e28d3bbbfdf922c74571ca685
Codevora <codevoratech@gmail.com>
2025-04-29 21:39:37 +0700

    fixing error

--- app/admin/dashboard/client-management/page.tsx
+++
@@ -1,5 +0,0 @@
-import ClientManagementPage from "@/components/layouts/Admin/Client/ClientManagementPage";
-
-export default function ClientManagement() {
- return <ClientManagementPage />;
-}
--- app/admin/dashboard/packages/page.tsx
+++
@@ -1,5 +0,0 @@
-import PackagePanel from "@/components/layouts/Admin/Packages/PackagePanel";
-
-export default function PackagesPage() {
- return <PackagePanel />;
-}
