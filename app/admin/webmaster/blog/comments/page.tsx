a49860dc763174e4a1a782b0d47b5639c23ac966
Codevora <codevoratech@gmail.com>
2025-05-01 02:57:29 +0700

    fix:error-blog-comment-page

--- app/admin/webmaster/blog/comments/page.tsx
+++ app/admin/webmaster/blog/comments/page.tsx
@@ -1,5 +1,6 @@
 import CommentViews from "@/components/layouts/Admin/WebMaster/Blog/CommentViews";
+import { Suspense } from "react";
 
 export default function AdminCommentsPage() {
- return <CommentViews/>
+ return <Suspense fallback={<div>Loading...</div>}><CommentViews /></Suspense>;
 }
